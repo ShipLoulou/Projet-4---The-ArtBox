33228551633256e138969c37e8d77bb6cf9e2473
Boileve <shiploulou.dev@gmail.com>
2024-09-25 11:49:50 +0200

    ajout fichier traitement qui vérifie et ajoute les données saisies dans le formulaire à la BDD

--- ajouter.php
+++ ajouter.php
@@ -1,6 +1,6 @@
-<?php require 'header.php'; ?>
+<?php require 'elements/header.php'; ?>
 
-<form action="traitement.php" method="POST">
+<form action="traitement/traitement.php" method="POST">
     <div class="champ-formulaire">
         <label for="titre">Titre de l'œuvre</label>
         <input type="text" name="titre" id="titre">
@@ -21,4 +21,4 @@
     <input type="submit" value="Valider" name="submit">
 </form>
 
-<?php require 'footer.php'; ?>
+<?php require 'elements/footer.php'; ?>
