f5b453b9d7eae984a714bd97ffd6c6b68ea0a6e0
Boileve <shiploulou.dev@gmail.com>
2024-09-25 11:27:37 +0200

    MAJ page oeuvre - connexion bdd

--- oeuvre.php
+++ oeuvre.php
@@ -1,19 +1,25 @@
 <?php
     require 'elements/header.php';
-    require 'data/oeuvres.php';
-
+    // require 'data/oeuvres.php';
+    require 'data/bdd.php';
+    
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
     }
 
+    $requete = connexion();
+    $response = $requete->prepare('SELECT * FROM oeuvres WHERE id = ?');
+    $response->execute([$_GET['id']]);
+    $oeuvres = $response->fetchAll();
+
     $oeuvre = null;
 
     // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
+    foreach($oeuvres as $item) {
         // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
+        if($item['id'] === intval($_GET['id'])) {
+            $oeuvre = $item;
             break; // On stoppe le foreach si on a trouvé l'oeuvre
         }
     }
